dddb58634881e9f03743eb60afaecfc8bd950007
Yanis Stentzel <114388444+ebyaMoyaK@users.noreply.github.com>
2026-01-31 14:27:18 +0100

    R working

--- Statistics:Report/SAE-Reporting/app.R
+++ Statistics:Report/SAE-Reporting/app.R
@@ -1,51 +1,21 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+      sidebarPanel(
+        selectInput("dist", "Distribution", choices = c("Exponentielle" = "exp", "Bernoulli" = "bern")),
+        sliderInput("param", "Paramètre", min = 0, max = 1, value = 0.5, step = 0.1)
+      ),
+      mainPanel(plotOutput("plot"))
     )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+  )
+  server <- function(input, output) {
+    output$plot <- renderPlot({
+      p <- input$param
+            switch(input$dist,
+             "exp" = hist(rexp(1000, rate = p), main = "Exponentielle"),
+             "bern" = barplot(table(rbinom(1000, 1, p)), main = "Bernoulli")
+      )
     })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+  }
+shinyApp(ui,server)
